0c55f048daac86e555d98a5d7d20bcd8cd94f876
lgraziano <leandro.graziano@epfl.ch>
2017-11-01 10:59:50 +0100

    update class Program

--- src/main/Program.java
+++ src/main/Program.java
@@ -5,12 +5,15 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[][] pattern = Helper.read("Pattern.png");
-		int[][] array = Helper.read("Image.png");
+		//must change path
+		int[][] pattern = Helper.read("C:\\Users\\Leandro\\Downloads\\charlie\\images\\pattern.png");
+		int[][] array = Helper.read("C:\\Users\\Leandro\\Downloads\\charlie\\images\\image.png");
 		double[][] distance = DistanceBasedSearch.distanceMatrix(pattern, array);
 		int[] coords = Collector.findBest(distance, true);
-		//Helper.drawBox(coords[1], coords[2], w, h, dst);
-		//Helper.show(array, title);
+		System.out.println(coords[0]);
+		System.out.println(coords[1]);
+		Helper.drawBox(coords[0], coords[1], pattern[0].length, pattern.length, array);
+		Helper.show(array, "image trouvée");
 	}
 
 }
