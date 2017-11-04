93f9d1f0f899d61eb9216cbfa2c541e5c3fe62cf
Luca Bataillard <lucabad@bluewin.ch>
2017-11-04 20:23:29 +0100

    fix ArrayOutOfBoundsException in wrapping methods

--- src/main/SimilarityBasedSearch.java
+++ src/main/SimilarityBasedSearch.java
@@ -49,7 +49,42 @@ public class SimilarityBasedSearch {
 	
 	
 	/**
-	 * Computes the Normalized Cross Correlation of a gray-scale pattern if positioned
+	 * Computes the mean value of a gray-scale pattern in an image
+	 * @param matrix : a 2D double array, the gray-scale Image
+	 * @param row : the y coordinate of the upper left corner of the pattern
+	 * @param col : the x coordinate of the upper left corner of the pattern
+	 * @param width : the width of the pattern
+	 * @param height : the height of the pattern
+	 * @param strategy : if equal to "wrap" or "mirror", will execute respective strategy. Default method otherwise
+	 * @return a double value between 0 and 255 which is the mean value
+	 */
+	static double windowMean(double[][] image, int row, int col, int width, int height, String strategy) {
+		
+		assert image.length > 0 && image[0].length > 0;
+		assert row + width < image.length && col + height < image[0].length;
+
+		if (!strategy.equals("wrap") && !strategy.equals("mirror")) {
+			return windowMean(image, row, col, width, height);
+		}
+		
+		double avg = 0;
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (strategy.equals("wrap")) {
+					avg += image[(i + row) % image.length][(j + col) % image[0].length];
+				} else {
+					avg += image[mirrorIndex(i + row, image.length)][mirrorIndex(j + col, image[0].length)];
+				}
+			}
+		}
+		
+		avg = avg/(width*height);
+		return avg;
+	}
+
+	
+	/**
+	 * Computes the Normalized Cross Correlation of a gray-scale pattern if positioned, according to a given strategy
 	 * at the provided row, column-coordinate in a gray-scale image
 	 * @param row : a integer, the row-coordinate of the upper left corner of the pattern in the image.
 	 * @param column : a integer, the column-coordinate of the upper left corner of the pattern in the image.
@@ -91,12 +126,67 @@ public class SimilarityBasedSearch {
 		}
 		
 		double d = imageSum * patternSum;
-		
 		if (d == 0) {
 			return -1;
 		} 
 		
 		return sum/Math.sqrt(d); 
+	}
+	
+	/**
+	 * Computes the Normalized Cross Correlation of a gray-scale pattern if positioned, according to a given strategy
+	 * at the provided row, column-coordinate in a gray-scale image
+	 * @param row : a integer, the row-coordinate of the upper left corner of the pattern in the image.
+	 * @param column : a integer, the column-coordinate of the upper left corner of the pattern in the image.
+	 * @param pattern : an 2D array of doubles, the gray-scale pattern to find
+	 * @param image : an 2D array of double, the gray-scale image where to look for the pattern
+	 * @param strategy : if equal to "wrap" or "mirror", will execute respective strategy. Default method otherwise
+	 * @return a double, the Normalized Cross Correlation value at position (row, col) between the pattern and the part of
+	 * the base image that is covered by the pattern, if the pattern is shifted by x and y.
+	 * should return -1 if the denominator is 0
+	 */
+	public static double normalizedCrossCorrelation(int row, int col, double[][] pattern, double[][] image, String strategy) {
+		assert pattern.length > 0 && pattern[0].length > 0;
+		assert image.length > 0 && image[0].length > 0;
+		
+		if (!strategy.equals("wrap") && !strategy.equals("mirror")) {
+			return normalizedCrossCorrelation(row, col, pattern, image);
+		}
+		
+		double imageMean = windowMean(image, row, col, pattern.length, pattern[0].length, strategy);
+		double patternMean = mean(pattern);
+		
+		double sum = 0;
+		double imageSum = 0;
+		double patternSum = 0;
+		
+		for (int i = 0; i < pattern.length; i++) {
+			for (int j = 0; j < pattern[0].length; j++) {				
+				if (strategy.equals("wrap")) {
+					int wrapI = (i + row) % image.length;
+					int wrapJ = (j + col) % image[0].length;
+					
+					sum += (image[wrapI][wrapJ] - imageMean) * (pattern[i][j] - patternMean);
+					imageSum += (image[wrapI][wrapJ] - imageMean) * (image[wrapI][wrapJ] - imageMean);
+				} else { 																					// if strategy == "mirror"
+					int mirrI = mirrorIndex(i + row, image.length);
+					int mirrJ = mirrorIndex(j + col, image[0].length);
+					
+					sum += (image[mirrI][mirrJ] - imageMean) * (pattern[i][j] - patternMean);
+					imageSum += (image[mirrI][mirrJ] - imageMean) * (image[mirrI][mirrJ] - imageMean);
+				}
+				
+				patternSum += (pattern[i][j] - patternMean) * (pattern[i][j] - patternMean);
+			}
+		}
+		
+		double d = imageSum * patternSum;
+		if (d == 0) {
+			return -1;
+		} 
+		
+		return sum/Math.sqrt(d); 
+		
 	}
 
 	
@@ -148,21 +238,25 @@ public class SimilarityBasedSearch {
 		
 		for (int i = 0; i < similarities.length; i++) {
 			for (int j = 0; j < similarities[i].length; j++) {
-				int row, col;
-			
-				if (strategy.equals("wrap")) {
-					row = i % similarities.length;
-					col = j % similarities[i].length;
-				} else { // if strategy == "mirror"
-					row = similarities.length - 2 - (i % similarities.length);
-					col = similarities.length - 2 - (j % similarities[i].length);
-				}
-				
-				similarities[i][j] = normalizedCrossCorrelation(row, col, pattern, image);
+				similarities[i][j] = normalizedCrossCorrelation(i, j, pattern, image, strategy);
 			}
 		}
 		
 		return similarities; 
+	}
+	
+	/**
+	 * Computes the mirrored index according to a maximum length: i.e. if index > maxLength, mirrors it
+	 * @param index
+	 * @param maxLength
+	 * @return an integer: the mirrored index
+	 */
+	public static int mirrorIndex(int index, int maxLength) {
+		if (index >= maxLength) {
+			return  maxLength - 2 - (index % maxLength);
+		}
+		
+		return index;
 	}
 
 }
