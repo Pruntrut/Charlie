225f64764c69f8e38a80045e2b87e3694b69324e
Luca Bataillard <lucabad@bluewin.ch>
2017-11-12 18:21:19 +0100

    update Program.java

--- src/main/Program.java
+++ src/main/Program.java
@@ -2,13 +2,63 @@ package main;
 
 public class Program {
 	public static void main(String args[]) {
-		int[][] food = Helper.read("images/charlie_beach.png");
-    	int[][] onions = Helper.read("images/charlie.png");
-    	double[][] distance = DistanceBasedSearch.distanceMatrix(onions, food); 	
-    	Helper.show(ImageProcessing.matrixToRGBImage(distance , 0, 255),
-    			"Distance");
-    	int[] p = Collector.findBest(distance, true);
-    	Helper.drawBox(p[0], p[1], onions[0].length, onions.length, food);
-    	Helper.show(food, "Found!");
+    	String charliePath = "images/charlie.png";
+    	String beachPath = "images/charlie_beach.png";
+    	
+    	findWithDistance(charliePath, beachPath);
+    	findWithSimilarity(charliePath, beachPath);
+    	
+    	compareDistanceAndSimilarity();
 	}
+	
+	public static void findWithDistance(String patternPath, String imagePath) {
+		System.out.println("Finding " + patternPath + " using distance based seach");
+    	int[][] pattern = Helper.read(patternPath);
+    	int[][] image = Helper.read(imagePath);
+    	
+    	double[][] distance = DistanceBasedSearch.distanceMatrix(pattern, image); 
+    	int[] coords = Collector.findBest(distance, true);
+    	
+    	System.out.println("Done!");
+    	showImageWithBox(image, pattern, coords, "Found with Distance");
+   	}
+	
+	public static void findWithSimilarity(String patternPath, String imagePath) {
+		System.out.println("Finding "+ patternPath +" using similarity based search");
+		int[][] pattern = Helper.read(patternPath);
+    	int[][] image = Helper.read(imagePath);
+		double[][] patternGrey = ImageProcessing.toGray(pattern);
+		double[][] imageGrey = ImageProcessing.toGray(image);
+		
+		double[][] similarity = SimilarityBasedSearch.similarityMatrix(patternGrey, imageGrey);
+		int[] coords = Collector.findBest(similarity, false);
+		
+		System.out.println("Done!");
+		showImageWithBox(image, pattern, coords, "Found with Similarity");
+	}
+	
+	public static void showImageWithBox(int[][] image, int[][] pattern,int[] coords, String message) {
+		Helper.drawBox(coords[0], coords[1], pattern[0].length, pattern.length, image);
+		Helper.show(image, message);
+	}
+	
+	public static void compareDistanceAndSimilarity() {
+		System.out.println("Compare Distance and Similarity based search");
+		int[][] image = Helper.read("images/image-dark.png");
+		int[][] pattern = Helper.read("images/pattern.png");
+		double[][] imageGray = ImageProcessing.toGray(image);
+		double[][] patternGray = ImageProcessing.toGray(pattern);
+		
+		double[][] distance = DistanceBasedSearch.distanceMatrix(pattern, image);
+		double[][] similarity = SimilarityBasedSearch.similarityMatrix(patternGray, imageGray);
+		
+		int[] bestDist = Collector.findBest(distance, true);
+		int[] bestSim = Collector.findBest(similarity, false);
+
+		Helper.drawBox(bestDist[0], bestDist[1], pattern[0].length, pattern.length, image);    	
+    	Helper.drawBox(bestSim[0], bestSim[1], pattern[0].length, pattern.length, image, pattern[0].length/15, 0x00ff00);;
+    	Helper.show(image, "Compare Dist (red) and Sim (green)");
+    	
+    	System.out.println("Done!");
+    }
 }
